651b78fe584e14dacb3b6f7e9006f296eeed4d51
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-07-03 08:16:18 +0530

    integrate serializer

--- apis/views.py
+++ apis/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
-
+"""
 def studentViews(request):
-    student ={'id': 1, 'name': "Vikas", 'age': 20}
-        
+    student =Student.objects.all()
+    # print(student)#<QuerySet [<Student: test1>, <Student: test2>]>
+    # convert queryset to json.  --drf is serializing the queryset to json
+    tempstudent =list(student.values())
+    print(tempstudent)
+    return JsonResponse(tempstudent, safe=False)
     
-    return JsonResponse(student)
+"""
+@api_view(['GET'])
+def studentViews(request):
+    if request.method == 'GET':
+        student =Student.objects.all()
+        studentSerializer = StudentSerializer(student, many=True)
+        return Response(studentSerializer.data, status=status.HTTP_200_OK)
--- students/models.py
+++ students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    student_id = models.CharField(primary_key=True)
+    name = models.CharField(max_length=100)
+    branch = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
